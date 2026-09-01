f11bd7dd8e08fde26b48b45dbd36d1a88a68bd4c
Hugo <ballack.h@escolar.ifrn.edu.br>
2026-09-01 13:43:41 -0300

    mais páginas provisórias

--- barbearia_seu_inacio/views.py
+++ barbearia_seu_inacio/views.py
@@ -8,3 +8,18 @@ def index(request):
 
 def base(request):
     return render(request, "barbearia_seu_inacio/base.html")
+
+def servicos(request):
+    return render(request, "barbearia_seu_inacio/servicos.html")
+
+def contato(request):
+    return render(request, "barbearia_seu_inacio/contato.html")
+
+def galeria(request):
+    return render(request, "barbearia_seu_inacio/galeria.html")
+
+def agenda(request):
+    return render(request, "barbearia_seu_inacio/agenda.html")
+
+def sobre(request):
+    return render(request, "barbearia_seu_inacio/sobre.html")

64084a8f7f8132c5c627e5865948e93174f5deff
Hugo <ballack.h@escolar.ifrn.edu.br>
2026-09-23 15:45:54 -0300

    inicializando o crud da galeria

--- barbearia_seu_inacio/models.py
+++ barbearia_seu_inacio/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 from tinymce.models import HTMLField
-from .models import User
 
 class Postagem(models.Model):
-    imagem = models.ImageField(uploud_to="galeria")
+    imagem = models.ImageField(upload_to="galeria/")
     descricao = HTMLField()
     data = models.DateField(auto_now=True)
-    autor = models.ForeignKey(User, on_delete=models.CASCADE)
--- barbearia_seu_inacio/views.py
+++ barbearia_seu_inacio/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Postagem
+from .forms import PostagemForm
+from django.contrib.auth.decorators import login_required, permission_required
 
 def index(request):
     return render(request, "barbearia_seu_inacio/index.html")
@@ -12,8 +15,55 @@ def servicos(request):
 def contato(request):
     return render(request, "barbearia_seu_inacio/contato.html")
 
-def galeria(request):
-    return render(request, "barbearia_seu_inacio/galeria.html")
+@login_required
+@permission_required("barbearia_seu_inacio.view_postagem")
+def galeria(request, id_postagem):
+    return render(request, "barbearia_seu_inacio/galeria.html", context = {
+        "post": get_object_or_404(Postagem, id=id_postagem),
+    })
+
+@login_required
+@permission_required("barbearia_seu_inacio.add_postagem")
+def nova_postagem(request):
+    if request.method == "POST":
+        form = PostagemForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("index")
+    else:
+        form = PostagemForm()
+
+    return render(request, "blog/form_post.html", context = {
+        "form": form,
+    })
+
+
+@login_required
+@permission_required("barbearia_seu_inacio.change_postagem")
+def editar_postagem(request, id_post):
+    post = get_object_or_404(Postagem, id=id_post)
+    if request.method == "POST":
+        form = PostagemForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect("index")
+    else:
+        form = PostagemForm(instance=post)
+
+    return render(request, "blog/form_post.html", context = {
+        "form": form,
+        "is_editar": True,
+    })
+
+@login_required
+@permission_required("barbearia_seu_inacio.delete_postagem")
+def remover_postagem(request, id_post):
+    if request.method == "POST":
+        post = get_object_or_404(Post, id=id_post)
+        post.delete()
+        return redirect("index")
+    else:
+        return render(request, "blog/confirmar_remocao.html")
 
 def agenda(request):
     return render(request, "barbearia_seu_inacio/agenda.html")
